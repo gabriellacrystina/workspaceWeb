2c172be3f84b52c692e2b05ced4cc0f44015adf6
Gabriella Alves <gabriellacrystina@gmail.com>
2015-09-24 16:46:32 -0300

    PrjVendas - Funcionando
    O projeto vendas está finalizado, funcionando.
    Nele foi utilizado o conceito de sessao e requisiçao, Servlet, JSP, HTML
    utilizando IDE  Eclipse e servidor web Tomcat.

--- PrjVendas/src/Servlet/RelatorioVendasVendedor.java
+++ PrjVendas/src/Servlet/RelatorioVendasVendedor.java
@@ -1,11 +1,18 @@
 package Servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Modelo.Regiao;
+import Modelo.Venda;
+import Modelo.Vendedor;
 
 /**
  * Servlet implementation class RelatorioVendasVendedor
@@ -33,7 +40,18 @@ public class RelatorioVendasVendedor extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession sessao = request.getSession();
+		
+		ArrayList<Vendedor> listaVendedores;
+		listaVendedores = (ArrayList<Vendedor>)sessao.getAttribute("listaVendedor");
+		
+		if(listaVendedores != null){
+			int indice = Integer.parseInt(request.getParameter("vendedor"));
+			Vendedor vendedor = listaVendedores.get(indice);
+			
+			sessao.setAttribute("v", vendedor);
+		}
+		
+		request.getRequestDispatcher("RelatorioVendasVendedor.jsp").forward(request, response);
 	}
-
 }
